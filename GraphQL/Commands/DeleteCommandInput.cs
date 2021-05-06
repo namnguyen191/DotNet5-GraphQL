69d7b74ebc7ada9b38c816192aecfc40fa17cfc5
= <=>
2021-05-05 20:59:36 -0400

    Add completed project

---
+++ GraphQL/Commands/DeleteCommandInput.cs
@@ -0,0 +1,4 @@
+namespace CommanderGQL.GraphQL.Commands
+{
+    public record DeleteCommandInput(int Id);
+}
---
+++ GraphQL/Platforms/AddPlatformPayload.cs
@@ -0,0 +1,9 @@
+using CommanderGQL.Models;
+
+namespace CommanderGQL.GraphQL.Platforms
+{
+    public record AddPlatformPayload(Platform platform)
+    {
+
+    }
+}
